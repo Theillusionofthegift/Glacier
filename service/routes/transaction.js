e190e98712cef45de1957111c4ad84b144a2ea55
Kristin Jue <kristin.jue@gmail.com>
2021-01-30 21:42:30 -0800

    created PUT method

--- service/routes/transaction.js
+++ service/routes/transaction.js
@@ -4,9 +4,11 @@ const transactionsRouter = express.Router();
 const {transaction} = require('../data/transactionList');
 
 transactionsRouter.route('/')
+
     .get((req, res, next) => {
         res.json(Data.transactionsList);
     })
+
     .post((req, res, next) => {
         const searchResult = transaction.filter((t) => {
             let j = express.json(req.body);
@@ -15,34 +17,54 @@ transactionsRouter.route('/')
         })
         
         if (searchResult.length != 0) {
-            // if transaction already exists, don't create a new transaction and send CONFLICT status code
+            // if transaction already exists, 
+            // don't create a new transaction and send CONFLICT status code
             res.sendStatus(409); 
         } else {
-            // if transaction not found, create a new transaction and send CREATED status code
+            // if transaction not found, 
+            // create a new transaction and send CREATED status code
             res.sendStatus(201); 
         }
     });
 
 transactionsRouter.route('/:id')
+
     .get((req, res, next) => {
         const matchingTransaction = transaction.filter((t) => {
             return req.params['id'] === t.id;
         })
 
         if (matchingTransaction.length === 1) {
-            // if matching transaction if found, return transaction and OK status code
+            // if matching transaction if found, 
+            // return transaction and OK status code
             res.send(matchingTransaction[0]); 
             res.sendStatus(200);
         } else {
-            // if matching transaction not found, return NOT FOUND status code
+            // if matching transaction not found, 
+            // return NOT FOUND status code
             res.sendStatus(404);
         }
-    }
-    .put((req, res, next) => {
-        res.sendStatus(501);
     })
+
+    .put((req, res, next) => {
+        const searchResult = transaction.filter((t) => {
+            return req.params['id'] === t.id;
+        })
+
+        if (searchResult.length != 0) {
+            // if matching transaction if found, 
+            // update transaction and return OK status code
+            res.sendStatus(204);
+        } else {
+            // if matching transaction not found, 
+            // return NOT FOUND status code
+            res.sendStatus(404);
+        }
+    })
+
     .delete((req, res, next) => {
         res.sendStatus(501);
-    })
+    });
+
 
 module.exports = transactionRouter;
