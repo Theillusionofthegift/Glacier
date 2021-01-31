fda609bdfad9a2593a700ab63c4b0b3c8d9f98bf
zaks2318 <68169592+zaks2318@users.noreply.github.com>
2021-01-30 19:08:47 -0800

    add mock data for the group.

---
+++ service/data/mockData.js
@@ -0,0 +1,20 @@
+exports.transactionsData = [
+	{user_id: 1, buyer_id:2, seller_id:2,item_id:1},
+	{user_id: 2, buyer_id:4, seller_id:5,item_id:3},
+	{user_id: 3, buyer_id:5, seller_id: 4,item_id:4},
+	{user_id: 4, buyer_id:8, seller_id: 3,item_id:5},
+]
+
+exports.userList = [
+	{user_id: 1, name: "Aaron", status: "Buyer"},
+	{user_id: 2, name: "Spongebob", status: "Seller"},
+	{user_id: 3, name: "Naruto", status: "Seller"},
+	{user_id: 4, name: "Barney", status: "Buyer"}
+]
+
+exports.productList = [
+    {id: "1", name: 'a rake', price: 243.0, available: true },
+    {id: '2', name: 'a snake', price: 102.0, available: true },
+    {id: '3', name: 'a flake', price: 784.0, available: false },
+    {id: '4', name: 'a cake', price: 98.5, available: true },
+  ]
