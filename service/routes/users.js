542d39c99d73a4545b7bb998f9cc21c860872d5f
Aaron <imaaronacs@gmail.com>
2021-02-01 10:17:08 -0800

    finished routing

--- service/routes/users.js
+++ service/routes/users.js
@@ -11,22 +11,74 @@ userRoutes.route('/')
     })
     .post((req, res, next) => 
     {
-        next("An error");
+        const s = Data.userList.filter((param) => {
+            let i = express.json(req.body);
+            console.log(i.user_id); 
+            return i.user_id === param.user_id;
+        })
+
+        if(s.length != 0) 
+        {
+            res.status(409);
+        }
+        else 
+        {
+            res.sendStatus(201);
+        }
     });
 
 
 userRoutes.route('/:id')
-    .get((req, res, next) => 
+    .get((req, res, next) => cd De
     {
-        res.sendStatus(501);
+        const m = Data.userList.filter((param) => {
+            return req.params['user_id'] === param.user_id;
+        })
+
+
+        if(m.length === 1) 
+        {
+            res.status(200).send(match[0]);
+        }
+        else 
+        {
+            res.sendStatus(404);
+        }
+
     })
     .put((req, res, next) => 
     {
-        res.sendStatus(501);
+        const s = Data.userList.filter((param) => {
+            return req.params['user_id'] === param.user_id;
+        })
+
+        if(s.length != 0) 
+        {
+            //update the user
+            res.sendStatus(204);
+        }
+        else 
+        {
+            //the user is not found send appropriate status
+            res.sendStatus(404);
+        }
+
     })
     .delete((req, res, next) => 
     {
-        res.sendStatus(501);
+        const s = Data.userList.filter((param) => {
+            return req.params['user_id'] === param.user_id;
+        })
+
+        if(s.length != 0) 
+        {
+            //if the user is found then delete it
+            res.sendStatus(204);
+        }
+        else 
+        {
+            res.sendStatus(404);
+        }
     });
 
 
