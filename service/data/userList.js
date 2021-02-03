f10b9a96a91002006557172c5187a0226e0f1842
Dylan Eastridge <49111933+Theillusionofthegift@users.noreply.github.com>
2021-02-02 16:46:01 -0800

    Delete userList.js

--- service/data/userList.js
+++
@@ -1,6 +0,0 @@
-exports.userList = [
-	{id: '1', name: "Aaron", status: "Buyer" },
-	{id: '2', name: "Spongebob", status: "Seller" },
-	{id: '3', name: "Naruto", status: "Seller" },
-	{id: '4', name: "Barney", status: "Buyer" }
-]
