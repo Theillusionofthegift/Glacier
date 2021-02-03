b8f5009439541607c45450557460a1ba9d2d2d2d
Dylan Eastridge <49111933+Theillusionofthegift@users.noreply.github.com>
2021-02-02 16:45:56 -0800

    Delete productList.js

--- service/data/productList.js
+++
@@ -1,9 +0,0 @@
-exports.productList = [
-  {id: "1", name: 'a rake', price: 243.0, available: true },
-  {id: '2', name: 'a snake', price: 102.0, available: true },
-  {id: '3', name: 'a flake', price: 784.0, available: false },
-  {id: '4', name: 'a cake', price: 98.5, available: true },
-]
-
-
-
