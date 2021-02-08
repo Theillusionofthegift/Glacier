3f70a1454df8b75b4b83ba0c0f6bcde21286e95a
Kristin Jue <kristin.jue@gmail.com>
2021-02-07 20:52:11 -0800

    added placeholders for short and long descriptions

--- client/src/views/ViewProductDescription.js
+++ client/src/views/ViewProductDescription.js
@@ -8,28 +8,52 @@ import './viewProduct.css';
 export default function ViewProductDescription(props) {
     return(
         <div>
-            <div className="view">
+            <div className="carousel">
                 <Carousel>
                     <Carousel.Item>
                         <img src={product} alt='productView1'/>
                         <Carousel.Caption>
                             <h3>First View</h3>
+                            <p>Short Description Here</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img src={product} alt='productView2'/>
                         <Carousel.Caption>
                             <h3>Second View</h3>
+                            <p>Short Description Here</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img src={product} alt='productView3'/>
                         <Carousel.Caption>
                             <h3>Third View</h3>
+                            <p>Short Description Here</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={product} alt='productView3'/>
+                        <Carousel.Caption>
+                            <h3>Fourth View</h3>
+                            <p>Short Description Here</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={product} alt='productView3'/>
+                        <Carousel.Caption>
+                            <h3>Fifth View</h3>
+                            <p>Short Description Here</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
+                
+            </div>
+            <div className="basicDescription">
+                <h2>Price $</h2>
                 <Button type="button">Send Message!</Button>
+            </div>
+            <div className="detailedDescription">
+                <p>Long Description Here</p>
             </div>
         </div>
     )
