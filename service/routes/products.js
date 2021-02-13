7afdb6a8fda94ea38e0cfa028a3f636110cb7cc4
Dylan <theillusionofthegift@gmail.com>
2021-02-12 23:20:17 -0800

    updated put and delete for product router

--- service/routes/products.js
+++ service/routes/products.js
@@ -9,8 +9,8 @@ const productController = require('../controllers/productController')
 productsRouter.route('/')
     .get((req, res, next) => {
         Product.find({}, (err, products) => {
-            if (err) {next("Something Went Wrong!")}
-            else { res.send(products)}
+            if (err) { next("Something Went Wrong!") }
+            else { res.send(products) }
         })
     })
     .post(productController.createProduct);
@@ -24,18 +24,30 @@ productsRouter.route('/:id')
             else if (products) { res.send(products) }
             // on the other hand, if it is null...
             else { res.sendStatus(404) }
-          })
+        })
     })
-    
-    .put((req, res, next) => { 
-        const options = {validate: true};
-        Product.findByIdAndUpdate(req.params.id, req.body,options, (err, product) => {
-            if (err) {next('Something Went Wrong!')}
-            else {res.status(200).send(product) }
+
+    .put((req, res, next) => {
+        const options = { validate: true };
+        Product.findByIdAndUpdate(req.params.id, req.body, options, (err, product) => {
+            if (err) {
+                next('Something Went Wrong!')
+            } else {
+                Product.findById(req.params.id, (err, product) => {
+                    res.send(product)
+                })
+            }
         })
     })
     .delete((req, res, next) => {
-
-    });
+        User.findByIdAndDelete(req.params.id, (err, user) => {
+            if (err) { next(err) }
+            else if (user) {
+                res.sendStatus(204);
+            } else {
+                res.status(404).send({ error: `Couldn't find user with id ${req.params.id}` });
+            }
+        });
+    })
 
 module.exports = productsRouter;
