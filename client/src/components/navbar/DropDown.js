97eb61f0d223e0adc0227b051fe0a0817f52415b
Dylan <theillusionofthegift@gmail.com>
2021-02-13 21:19:40 -0800

    fixed DropDown Button Links

--- client/src/components/navbar/DropDown.js
+++ client/src/components/navbar/DropDown.js
@@ -1,13 +1,14 @@
 import { React } from 'react'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
+import {Link} from 'react-router-dom'
 
 export default function DropDown(props) {
   return (
     <DropdownButton size="lg" id="cat" title="Categories">
-      <Dropdown.Item href="/">Category 1</Dropdown.Item>
-      <Dropdown.Item href="/">Category 2</Dropdown.Item>
-      <Dropdown.Item href="/">Category 3</Dropdown.Item>
+      <Dropdown.Item as={Link} to="/">Category 1</Dropdown.Item>
+      <Dropdown.Item as={Link} to="/">Category 2</Dropdown.Item>
+      <Dropdown.Item as={Link} to="/">Category 3</Dropdown.Item>
     </DropdownButton>
   )
 }
