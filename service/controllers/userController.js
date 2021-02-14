619e62c525e00769e09de7ac26d8b8434e79c895
Kristin Jue <kristin.jue@gmail.com>
2021-02-13 23:19:51 -0800

    created userController.js file

---
+++ service/controllers/userController.js
@@ -0,0 +1,36 @@
+const dateFns = require("date-fns");
+
+const User = require("../models/users");
+
+exports.createUser = (req, res, next) => {
+
+    // make sure user name isn't blank
+    if (req.body.userName.trim().length === 0) {
+        res.status(400).send({error:"User name cannot be blank"});
+    }
+
+    // make sure email isn't blank
+    if (req.body.email.trim().length === 0) {
+        res.status(400).send({error:"Email cannot be blank"});
+    }
+
+    const user = {
+        userName: req.body.userName,
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        bio: req.body.bio,
+    };
+
+
+
+    User.create(user)
+    .then((user) => {
+        res.send({userId: user._id });
+    })
+    .catch((err) => {
+        console.log(err);
+        next(err);
+    });
+};
+
