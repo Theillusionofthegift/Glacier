086f81f4614646418f47baf5e6166027ffa763f4
Dylan Eastridge <49111933+Theillusionofthegift@users.noreply.github.com>
2021-02-14 23:48:02 -0800

    Update MessageView.js

--- client/src/views/MessageView.js
+++ client/src/views/MessageView.js
@@ -1,74 +1,83 @@
-import './message.css';
-import Container from 'react-bootstrap/Container'
-import Toast from 'react-bootstrap/Toast'
-import InputGroup from 'react-bootstrap/InputGroup'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
+import React, { useState } from react
+import { useAuth0 } from '@auth0auth0-react'
+import Container from 'react-bootstrapContainer'
+import axios from axios;
+import MessageProvider from '..componentsmessagesMessageProvider'
+import InputGroup from 'react-bootstrapInputGroup'
+import FormControl from 'react-bootstrapFormControl'
+import Button from 'react-bootstrapButton'
 
-function HomePage() {
+function MessageView() {
+
+    const defaultMessageValues = {
+        user: "" ,
+        message: "" 
+    };
+
+    const [messageValues, setMessageValues] = useState(defaultMessageValues);
+    const [success, setSuccess] = useState(false);
+    const { user} = useAuth0();
+
+
+
+
+    const handleInputChange = (event) = {
+        const { name, value } = event.target;
+        console.log(`name ${name} and value ${value}`);
+        setMessageValues({
+            ...messageValues,
+            [name]: value,
+        });
+        console.log(productFormValues);
+    };
+
+    const handleSubmit = (event) = {
+        event.preventDefault();
+        const { id } = useParams();
+        const requestConfig = {
+            url `httplocalhost4000apiv1conversations${id}`,
+            method post,
+            headers { Content-Type applicationjson },
+            data {
+                user: user.sub,
+                message: messageValues.message
+            },
+        };
+
+        axios(requestConfig)
+            .then((response) = {
+                setSuccess(true);
+                console.log(`Item Created ${response.data}`);
+            })
+            .catch((err) = {
+                console.log(`We should really handle the error ${err}`);
+            });
+    };
+
+
+
     return (
-        <Container className="pt-5">
-            <Toast>
-                <Toast.Header>
-                    <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-                    <strong className="mr-auto">Seller</strong>
-                    <small>10 min ago</small>
-                </Toast.Header>
-                <Toast.Body>Hello.</Toast.Body>
-            </Toast>
-            <Toast>
-                <Toast.Header>
-                    <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-                    <strong className="mr-auto">Seller</strong>
-                    <small>9 min ago</small>
-                </Toast.Header>
-                <Toast.Body>I can do 400.</Toast.Body>
-            </Toast>
-            <div
-                aria-live="polite"
-                aria-atomic="true"
-                style={{
-                    position: 'relative',
-                    minHeight: '200px',
-                }}
-            >
-                <div
-                    style={{
-                        position: 'absolute',
-                        top: 0,
-                        right: 0,
-                    }}
-                >
-                    <Toast>
-                        <Toast.Header>
-                            <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-                            <strong className="mr-auto">Buyer</strong>
-                            <small>2 seconds ago</small>
-                        </Toast.Header>
-                        <Toast.Body>How about 350?</Toast.Body>
-                    </Toast>
-                    <Toast>
-                        <Toast.Header>
-                            <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-                            <strong className="mr-auto">Buyer</strong>
-                            <small>just now</small>
-                        </Toast.Header>
-                        <Toast.Body>Where are you located?</Toast.Body>
-                    </Toast>
-                </div>
-            </div>
-            <InputGroup className="mb-3">
-                <FormControl
-                    placeholder="Your Message Here..."
-                    aria-label="message "
-                    aria-describedby="basic-addon2"
-                />
+        <Container className=pt-5>
+            <MessageProvider />
+            <InputGroup className=mb-3>
+                <FormControl>
+                    placeholder=Your Message Here...
+                    aria-label=message 
+                    aria-describedby=basic-addon2
+                    name=message
+                    onChange={handleInputChange}
+                </FormControl>
                 <InputGroup.Append>
-                    <Button variant="outline-secondary">Send</Button>
+                    <Button 
+                    variant=outline-secondary
+                    type=submit
+                    onClick={handleSubmit}
+                    >
+                        Send</Button>
                 </InputGroup.Append>
             </InputGroup>
         </Container>
     );
 }
 
-export default HomePage;
+export default MessageView;
