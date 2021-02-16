ad21058b80208a96781ff2c5cdc4c25379681d22
Dylan Eastridge <49111933+Theillusionofthegift@users.noreply.github.com>
2021-02-15 22:00:31 -0800

    Update conversationController.js

--- service/controllers/conversationController.js
+++ service/controllers/conversationController.js
@@ -4,13 +4,14 @@ const Conversation = require("../models/conversations");
 
 exports.createConversation=(req, res, next)=>{
     
-    if(req.body.message==0){
-        res.status(400).send({error:"Do not send EMPTY message!!"})
+    //Check to see if conversation has users in the users array
+    if(req.body.users.length == 0){
+        res.status(400).send({error:"Conversation must have users!!"})
     }
 
     const conversation = {
-        user: req.body.user,
-        message: req.body.message,
+        users: req.body.users,
+        messages: req.body.messages,
     };
 
     Conversation.create(conversation)
