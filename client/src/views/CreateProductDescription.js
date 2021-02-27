056c92a377411d80c9def66f1b009f114dfac766
Dylan Eastridge <theillusionofthegift@gmail.com>
2021-02-27 13:34:10 -0800

    update to last commit

--- client/src/views/CreateProductDescription.js
+++ client/src/views/CreateProductDescription.js
@@ -110,59 +110,68 @@ export default function CreateProduct() {
                             onChange={handleInputChange}
                         />
                     </InputGroup>
-                    <DropdownButton
-                        as={InputGroup.Append}
-                        variant="outline-primary"
-                        title="Categories"
-                        value={productFormValues.categories}
-                        id="input-categories-2"
-                    >
-                        <Dropdown.Item >Appliances</Dropdown.Item>
-                        <Dropdown.Item >Arts and crafts</Dropdown.Item>
-                        <Dropdown.Item >Audio Equipment</Dropdown.Item>
-                        <Dropdown.Item >Baby and kids</Dropdown.Item>
-                        <Dropdown.Item >Beauty and health</Dropdown.Item>
-                        <Dropdown.Item >Bicycles</Dropdown.Item>
-                        <Dropdown.Item >Boats and marine</Dropdown.Item>
-                        <Dropdown.Item >Books and magazines</Dropdown.Item>
-                        <Dropdown.Item >Business equipment</Dropdown.Item>
-                        <Dropdown.Item >Campers and RVs</Dropdown.Item>
-                        <Dropdown.Item >Cars and trucks</Dropdown.Item>
-                        <Dropdown.Item >CDs and DVDs</Dropdown.Item>
-                        <Dropdown.Item >Cell phones</Dropdown.Item>
-                        <Dropdown.Item >Clothing and shoes</Dropdown.Item>
-                        <Dropdown.Item >Collectibles</Dropdown.Item>
-                        <Dropdown.Item >Computer equipment</Dropdown.Item>
-                        <Dropdown.Item >Electronics</Dropdown.Item>
-                        <Dropdown.Item >Exercise</Dropdown.Item>
-                        <Dropdown.Item >Farming</Dropdown.Item>
-                        <Dropdown.Item >Furniture</Dropdown.Item>
-                        <Dropdown.Item >Games and toys</Dropdown.Item>
-                        <Dropdown.Item >Home and garden</Dropdown.Item>
-                        <Dropdown.Item >Household</Dropdown.Item>
-                        <Dropdown.Item >Jewelry and accessories</Dropdown.Item>
-                        <Dropdown.Item >Motorcycles</Dropdown.Item>
-                        <Dropdown.Item >Musical instruments</Dropdown.Item>
-                        <Dropdown.Item >Pet supplies</Dropdown.Item>
-                        <Dropdown.Item >Photography</Dropdown.Item>
-                        <Dropdown.Item >Software</Dropdown.Item>
-                        <Dropdown.Item >Sports and outdoors</Dropdown.Item>
-                        <Dropdown.Item >Tickets</Dropdown.Item>
-                        <Dropdown.Item >TVs</Dropdown.Item>
-                        <Dropdown.Item >Video equipment</Dropdown.Item>
-                        <Dropdown.Item >Video games</Dropdown.Item>
 
-                    </DropdownButton>
+                    <InputGroup>
+                        <FormControl
+                            placeholder="Recipient's username"
+                            aria-label="Recipient's username"
+                            aria-describedby="basic-addon2"
+                            name="categories"
+                            value={productFormValues.categories}
+                            onChange={handleInputChange}
+                        />
+                        <DropdownButton
+                            as={InputGroup.Append}
+                            variant="outline-primary"
+                            title="Categories"
+                            id="input-categories-2"
+                        >
+                            <Dropdown.Item >Appliances</Dropdown.Item>
+                            <Dropdown.Item >Arts and crafts</Dropdown.Item>
+                            <Dropdown.Item >Audio Equipment</Dropdown.Item>
+                            <Dropdown.Item >Baby and kids</Dropdown.Item>
+                            <Dropdown.Item >Beauty and health</Dropdown.Item>
+                            <Dropdown.Item >Bicycles</Dropdown.Item>
+                            <Dropdown.Item >Boats and marine</Dropdown.Item>
+                            <Dropdown.Item >Books and magazines</Dropdown.Item>
+                            <Dropdown.Item >Business equipment</Dropdown.Item>
+                            <Dropdown.Item >Campers and RVs</Dropdown.Item>
+                            <Dropdown.Item >Cars and trucks</Dropdown.Item>
+                            <Dropdown.Item >CDs and DVDs</Dropdown.Item>
+                            <Dropdown.Item >Cell phones</Dropdown.Item>
+                            <Dropdown.Item >Clothing and shoes</Dropdown.Item>
+                            <Dropdown.Item >Collectibles</Dropdown.Item>
+                            <Dropdown.Item >Computer equipment</Dropdown.Item>
+                            <Dropdown.Item >Electronics</Dropdown.Item>
+                            <Dropdown.Item >Exercise</Dropdown.Item>
+                            <Dropdown.Item >Farming</Dropdown.Item>
+                            <Dropdown.Item >Furniture</Dropdown.Item>
+                            <Dropdown.Item >Games and toys</Dropdown.Item>
+                            <Dropdown.Item >Home and garden</Dropdown.Item>
+                            <Dropdown.Item >Household</Dropdown.Item>
+                            <Dropdown.Item >Jewelry and accessories</Dropdown.Item>
+                            <Dropdown.Item >Motorcycles</Dropdown.Item>
+                            <Dropdown.Item >Musical instruments</Dropdown.Item>
+                            <Dropdown.Item >Pet supplies</Dropdown.Item>
+                            <Dropdown.Item >Photography</Dropdown.Item>
+                            <Dropdown.Item >Software</Dropdown.Item>
+                            <Dropdown.Item >Sports and outdoors</Dropdown.Item>
+                            <Dropdown.Item >Tickets</Dropdown.Item>
+                            <Dropdown.Item >TVs</Dropdown.Item>
+                            <Dropdown.Item >Video equipment</Dropdown.Item>
+                            <Dropdown.Item >Video games</Dropdown.Item>
+                        </DropdownButton>
+                    </InputGroup>
 
-                    <div className="mb-3">
-                        <Form.File id="formcheck-api-regular">
-                            <Form.File.Label>Regular file input</Form.File.Label>
-                            <Form.File.Input />
-                        </Form.File>
-                    </div>
-                    <Button type="submit" onClick={handleSubmit}>Submit form</Button>
+                        <div className="mb-3">
+                            <Form.File id="formcheck-api-regular">
+                                <Form.File.Label>Regular file input</Form.File.Label>
+                                <Form.File.Input />
+                            </Form.File>
+                        </div>
+                        <Button type="submit" onClick={handleSubmit}>Submit form</Button>
                 </div>
-            </div>
+                </div>
         )
     }
 }
