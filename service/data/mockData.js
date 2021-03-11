f1700618e2fc1c962033a551804dbb1d304bb053
Kristin Jue <kristin.jue@gmail.com>
2021-03-10 23:26:36 -0800

    update the mockData file

--- service/data/mockData.js
+++ service/data/mockData.js
@@ -1,127 +1,274 @@
-exports.userList = [
-  {
-    userName: 'FeistyDoom',
-    email: 'grahamosborn@powernet.com',
-    firstName: 'Jackson',
-    lastName: 'Hodge',
-    dateJoined: { type: Date, default: Date.now },
-    active: true,
-    bio: 'Consequat amet ea aliqua minim ipsum amet. Veniam nostrud incididunt officia in laborum ea sit veniam officia non do. Aliqua do duis ad sunt in duis laboris deserunt aute reprehenderit officia eiusmod. Pariatur nostrud in consequat ad velit dolor laborum id Lorem non dolore in.',
-  },
-  {
-    userName: 'FreakTimesAce',
-    email: 'williewebb@dragbot.com',
-    firstName: 'Tasha',
-    lastName: 'Mckee',
-    dateJoined: { type: Date, default: Date.now },
-    active: true,
-    bio: 'Ut consequat culpa velit eiusmod consequat est nostrud adipisicing enim quis occaecat cillum. Qui ad adipisicing Lorem proident irure laborum ad reprehenderit velit aliqua occaecat. Et reprehenderit consectetur labore sint ad ullamco ipsum esse.',
-  },
-  {
-    userName: 'BoxPanet',
-    email: 'lambhays@megall.com',
-    firstName: 'Hewitt',
-    lastName: 'Mueller',
-    dateJoined: { type: Date, default: Date.now },
-    active: true,
-    bio: 'Nostrud exercitation cillum occaecat quis velit labore. Labore cupidatat cillum anim esse pariatur in. Consectetur dolor eu laboris laboris elit magna qui anim adipisicing consectetur mollit officia.',
-  },
-  {
-    userName: 'LoveNumero',
-    email: 'gonzalezgarza@opticall.com',
-    firstName: 'Rhea',
-    lastName: 'Savage',
-    dateJoined: { type: Date, default: Date.now },
-    active: true,
-    bio: 'Lorem dolore exercitation officia minim adipisicing eu cupidatat et dolore Lorem anim. Tempor eiusmod quis sint ut nostrud reprehenderit adipisicing eu cupidatat. Ad dolore deserunt esse do adipisicing tempor est laboris laboris sint dolore consequat labore ex.',
-  },
-  {
-    userName: '2cuteBe2hot',
-    email: 'ginacross@gazak.com',
-    firstName: 'Miranda',
-    lastName: 'Terrell',
-    dateJoined: { type: Date, default: Date.now },
-    active: true,
-    bio: 'Lorem ut mollit aute elit ex dolor. Nisi labore ad nisi reprehenderit in commodo reprehenderit reprehenderit mollit. Consequat consectetur labore eiusmod laboris ut.',
-  },
-  {
-    userName: 'CampyCrisp',
-    email: 'levyvelasquez@dreamia.com',
-    firstName: 'Payne',
-    lastName: 'White',
-    dateJoined: { type: Date, default: Date.now },
-    active: true,
-    bio: 'Veniam labore officia Lorem deserunt quis exercitation deserunt ad irure ex eu cillum. Amet ut nulla cupidatat elit nostrud enim reprehenderit.',
-  },
-  {
-    userName: 'IffyThief',
-    email: 'welchgay@gadtron.com',
-    firstName: 'Monica',
-    lastName: 'Thompson',
-    dateJoined: { type: Date, default: Date.now },
-    active: true,
-    bio: 'Sint id minim cillum labore culpa laboris dolor ex elit incididunt ipsum quis. Aliquip officia consequat reprehenderit et veniam. Amet enim minim eiusmod ipsum sit esse ullamco enim irure qui officia.',
-  },
-];
+// just a comment here
 
-exports.productList = [
-  {
-    prodName: 'GoPro Hero 4',
-    price: 243.0,
-    available: true,
-    description: 'Laboris esse do qui consequat sint ut voluptate nisi reprehenderit id nulla. Pariatur nostrud et eu culpa voluptate ipsum excepteur nostrud sint.',
-    categories: ['Home and Garden', 'Outdoor'],
+exports.userList = [{
+  _id: {
+    $oid: "60398d0db81ad7613b9a7fc3"
   },
-  {
-    prodName: 'Samsung Galaxy s10',
-    price: 102.0,
-    available: true,
-    description: '"Minim laboris exercitation enim consectetur aute ad dolore dolore deserunt ad amet fugiat esse elit. Duis laborum laboris incididunt incididunt exercitation voluptate ex in ut enim.',
-    categories: ['Pets', 'Reptiles'],
+  userType: "user",
+  active: true,
+  auth0Id: "106044809483574283097",
+  userName: "Dylan",
+  email: "theillusionofthegift@gmail.com",
+  firstName: "test",
+  lastName: "test",
+  bio: "gsaghjasofg",
+  dateJoined: {
+    $date: "2021-02-27T00:06:37.661Z"
   },
-  {
-    prodName: 'PS5',
-    price: 784.0,
-    available: true,
-    description: 'Ad excepteur pariatur aliquip sit labore non. Sint incididunt proident veniam consequat ea fugiat sit cillum amet commodo voluptate.',
-    categories: ['Household', 'Misc'],
+  __v: 0
+},{ 
+  _id: { 
+    $oid: "60398d55b81ad7613b9a7fc4"
   },
-  {
-    prodName: 'Fender Strtatocaster Guitar',
-    price: 98.5,
-    available: true,
-    description: 'Sunt exercitation exercitation labore enim pariatur ea consequat excepteur ipsum ad do eiusmod enim esse. Veniam aute labore mollit ullamco deserunt cupidatat ut ea proident eu fugiat ut. Eu excepteur Lorem dolor amet do amet ipsum ex eu non. Duis quis irure laborum incididunt.',
-    categories: ['Food', 'General'],
+  userType: "user",
+  active: true,
+  auth0Id: "60398d438756df0069a45d97",
+  userName: "MrBlah",
+  email: "blah2@testtest.com",
+  firstName: "Balh",
+  lastName: "Lash",
+  bio: "BlahBlahBlah",
+  dateJoined: {
+    $date: "2021-02-27T00:07:49.550Z"
   },
-  {
-    prodName: 'Nike Shoes',
-    price: 243.0,
-    available: true,
-    description: 'Excepteur nisi voluptate fugiat fugiat excepteur pariatur aliqua duis adipisicing. Voluptate exercitation sint do nostrud est voluptate adipisicing mollit aute magna est qui nostrud. Elit et veniam non duis elit duis incididunt id elit do eiusmod eu commodo est.',
-    categories: ['Home and Garden', 'Outdoor'],
+  __v: 0
+},{
+  _id: {
+    $oid: "60431c0f1eff5f8d3e6923c0"
   },
-  {
-    prodName: 'MacBook Pro',
-    price: 102.0,
-    available: true,
-    description: 'Excepteur nisi voluptate fugiat fugiat excepteur pariatur aliqua duis adipisicing. Voluptate exercitation sint do nostrud est voluptate adipisicing mollit aute magna est qui nostrud. Elit et veniam non duis elit duis incididunt id elit do eiusmod eu commodo est.',
-    categories: ['Pets', 'Reptiles'],
+  userType: "user",
+  active: true,
+  auth0Id: "604319e3bd1ee3006a612d43",
+  userName: "Illusiom",
+  email: "coyotefootball55@ymail.com",
+  firstName: "Dylan",
+  lastName: "Eastridge",
+  bio: "asdfgasdfg",
+  dateJoined: {
+    $date: "2021-03-06T06:07:11.345Z"
   },
-  {
-    prodName: '3D Printer',
-    price: 784.0,
-    available: true,
-    description: 'Qui mollit quis elit veniam dolor eiusmod sit occaecat adipisicing consequat. In et aliquip amet aliqua. Aliqua elit dolore culpa excepteur commodo esse exercitation anim. Id cupidatat cupidatat commodo irure sit labore non pariatur esse non dolor sit laboris incididunt.',
-    categories: ['Household', 'Misc'],
+  __v: 0
+},{
+  _id: {
+    $oid: "6045b3b7015e0085212d8722"
   },
-  {
-    prodName: 'Condenser Microphone',
-    price: 98.5,
-    available: true,
-    description: 'Amet commodo culpa magna amet ex deserunt dolore eiusmod eiusmod ullamco eu in qui officia. Voluptate ipsum dolore sunt consequat exercitation pariatur aute. Deserunt enim proident officia duis qui cupidatat nulla sunt officia irure sit.',
-    categories: ['Food', 'General'],
+  userType: "user",
+  active: true,
+  auth0Id: "60397a55eb4d600070feec63",
+  userName: "Dylan",
+  email: "dylan.eastridge@seattlecolleges.edu",
+  firstName: "Eastridge",
+  lastName: "Eastridge",
+  bio: "Testtest",
+  dateJoined: {
+    $date: "2021-03-08T05:18:47.892Z"
   },
-];
+  __v: 0
+},{
+  _id: {
+    $oid: "6047cfbd82f2c42b0dca3319"
+  },
+  userType: "user",
+  active: true,
+  auth0Id: "6047cfb03fe3250069f5a5d1",
+  userName: "tEST",
+  email: "testy@testtest.com",
+  firstName: "Test",
+  lastName: "Test",
+  bio: "gasfkjgasdklJF",
+  dateJoined: {
+    $date: "2021-03-09T19:42:53.174Z"
+  },
+  __v: 0
+},{
+  _id: {
+    $oid: "6047d05783fa0c62b1653bb4"
+  },
+  userType: "user",
+  active: true,
+  auth0Id: "6046c74db5d39d007048742c",
+  userName: "testuser",
+  email: "testeremail@test.com",
+  firstName: "first",
+  lastName: "last",
+  bio: "test bio",
+  dateJoined: {
+    $date: "2021-03-09T19:45:27.571Z"
+  },
+  __v: 0
+},{
+  _id: {
+    $oid: "60498ebdbffd27a023e754cd"
+  },
+  userType: "user",
+  active: true,
+  auth0Id: "114285589784471963527",
+  userName: "",
+  email: "kristin.jue@gmail.com",
+  firstName: "",
+  lastName: "",
+  bio: "",
+  dateJoined: {
+    $date: "2021-03-11T03:30:05.001Z"
+  },
+  __v: 0
+}];
+
+exports.productList = [{
+  _id: {
+    $oid: "6048848fdba1da7fddaa6ef5"
+  },
+  available: true,
+  prodName: "pair of rollerblades",
+  seller: "6046c74db5d39d007048742c",
+  price: 25,
+  description: "size 7",
+  category: "",
+  zipcode: {
+    _id: {
+      $oid: "6048848fdba1da7fddaa6ef6"
+    },
+    readable: "Pasadena, CA 91108, USA",
+    latitude: 34.1247921,
+    longitude: -118.1181199
+  },
+  postedDate: {
+    $date: "2021-03-10T08:34:23.860Z"
+  },
+  __v: 0
+},{
+  _id: {
+    $oid: "604949cbe99918973e5dff42"
+  },
+  available: true,
+  prodName: "armchair",
+  seller: "6046c74db5d39d007048742c",
+  price: 35,
+  description: "a nice comfy armchair",
+  category: "",
+  zipcode: {
+    _id: {
+      $oid: "604949cbe99918973e5dff43"
+    },
+    readable: "Seattle, WA 98125, USA",
+    latitude: 47.7170204,
+    longitude: -122.3009337
+  },
+  postedDate: {
+    $date: "2021-03-10T22:35:55.870Z"
+  },
+  __v: 0
+},{
+  _id: {
+    $oid: "604949f2e99918973e5dff49"
+  },
+  available: true,
+  prodName: "coat rack",
+  seller: "6046c74db5d39d007048742c",
+  price: 5,
+  description: "antique coat rack",
+  category: "",
+  zipcode: {
+    _id: {
+      $oid: "604949f2e99918973e5dff4a"
+    },
+    readable: "Seattle, WA 98115, USA",
+    latitude: 47.6849444,
+    longitude: -122.2982224
+  },
+  postedDate: {
+    $date: "2021-03-10T22:36:34.691Z"
+  },
+  __v: 0
+},{
+  _id: {
+    $oid: "60498e4ca39c578bcd135d4f"
+  },
+  available: true,
+  prodName: "Motorcyle",
+  seller: "106044809483574283097",
+  price: 4000,
+  description: "Very Fast Very Speedy",
+  category: "Motorcycles",
+  zipcode: {
+    "_id": {
+      "$oid": "60498e4ca39c578bcd135d50"
+    },
+    "readable": "Seattle, WA 98103, USA",
+    "latitude": 47.6600087,
+    "longitude": -122.3425575
+  },
+  "postedDate": {
+    "$date": "2021-03-11T03:28:12.168Z"
+  },
+  "__v": 0
+},{
+  "_id": {
+    "$oid": "60498fb051bae2919bae80ab"
+  },
+  "available": true,
+  "prodName": "ps5",
+  "seller": "106044809483574283097",
+  "price": 7900048,
+  "description": "Test",
+  "category": "Electronics",
+  "zipcode": {
+    "_id": {
+      "$oid": "60498fb051bae2919bae80ac"
+    },
+    "readable": "Seattle, WA 98103, USA",
+    "latitude": 47.6600087,
+    "longitude": -122.3425575
+  },
+  "postedDate": {
+    "$date": "2021-03-11T03:34:08.665Z"
+  },
+  "__v": 0
+},{
+  "_id": {
+    "$oid": "604993ddb63fcaa6f84635db"
+  },
+  "available": true,
+  "prodName": "ps4",
+  "seller": "114285589784471963527",
+  "price": 300,
+  "description": "ps4",
+  "category": "Electronics",
+  "zipcode": {
+    "_id": {
+      "$oid": "604993ddb63fcaa6f84635dc"
+    },
+    readable: "Seattle, WA 98133, USA",
+    latitude: 47.7393027,
+    longitude: -122.3435335
+  },
+  postedDate: {
+    $date: "2021-03-11T03:51:57.185Z"
+  },
+  __v: 0
+},{
+  _id: {
+    $oid: "6049aa73d4010baa0838350e"
+  },
+  available: true,
+  prodName: "Bike",
+  seller: "114285589784471963527",
+  price: 35,
+  description: "navy bike",
+  category: "Bicycles",
+  zipcode: {
+    _id: {
+      "$oid": "6049aa73d4010baa0838350f"
+    },
+    readable: "Seattle, WA 98115, USA",
+    latitude: 47.6849444,
+    longitude: -122.2982224
+  },
+  postedDate: {
+    $date: "2021-03-11T05:28:19.697Z"
+  },
+  __v: 0
+}];
 
 exports.conversations = [
   {
