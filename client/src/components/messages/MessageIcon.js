09dd6bb07cdf9136d3eebc82d70b8bfa78e5a53c
Dylan Eastridge <theillusionofthegift@gmail.com>
2021-03-18 18:50:25 -0700

    formatt

--- client/src/components/messages/MessageIcon.js
+++ client/src/components/messages/MessageIcon.js
@@ -4,12 +4,12 @@ import messageBubble from "../../images/messagebubble.png"
 import { useAuth0 } from '@auth0/auth0-react'
 
 export default function MessageIcon() {
-const {isAuthenticated} = useAuth0();
+    const { isAuthenticated } = useAuth0();
 
-    if(isAuthenticated){
+    if (isAuthenticated) {
         return (
-            <Container style={{position: "fixed", right: "-900px", bottom:"0", paddingBottom: "10px"}}>
-                    <a href="/conversations/all"><Image src={messageBubble} alt='messages' roundedCircle/></a>
+            <Container style={{ position: "fixed", right: "-900px", bottom: "0", paddingBottom: "10px" }}>
+                <a href="/conversations/all"><Image src={messageBubble} alt='messages' roundedCircle /></a>
             </Container>
         );
     } else {
