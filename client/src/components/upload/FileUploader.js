cf7b7471847ab9187fdbbe48a2940d32726b5d79
Dylan Eastridge <theillusionofthegift@gmail.com>
2021-03-20 15:14:29 -0700

    redirect to home after file uploder completes

--- client/src/components/upload/FileUploader.js
+++ client/src/components/upload/FileUploader.js
@@ -5,6 +5,7 @@ import {
     Button,
     ProgressBar
 } from 'react-bootstrap'
+import { Redirect } from 'react-router';
 
 
 
@@ -12,6 +13,7 @@ export default function FileUploader(props) {
 
     const [imageFiles, setimageFiles] = useState([]);
     const [uploadProgress, setUploadProgress] = useState(0);
+    const [success, setSuccess] = useState(false);
     const [error, setError] = useState(null);
 
     const selectFile = (event) => {
@@ -28,6 +30,7 @@ export default function FileUploader(props) {
             UseFileUpload('images', imageFiles, progressUpdater, props.prodId)
                 .then((response) => {
                     setimageFiles([]);
+                    setSuccess(true);
                 })
                 .catch((err) => {
                     setError(err);
@@ -36,44 +39,49 @@ export default function FileUploader(props) {
         }
     };
 
-    return (
-        <>
-            <Container>
-                <label htmlFor="btn-upload">
-                    <input
-                        id="btn-upload"
-                        name="btn-upload"
-                        style={{ display: 'none' }}
-                        type="file"
-                        onChange={selectFile}
-                        multiple
-                    />
-                    <Button
-                        variant="primary"
-                        as= "span"
-                        className= "mb-3"
-                    >
-                        Choose up to 3 images:
-            </Button>
-                </label>
-                {imageFiles[0] ? <p>{imageFiles[0].name} <br/></p> : <p>Select a File</p>}
-                {imageFiles[1] ? <p>{imageFiles[1].name} <br/></p> : <p>Select a File</p>}
-                {imageFiles[2] ? <p>{imageFiles[2].name}</p> : <p>Select a File</p>}
+    if(success) {
+        return <Redirect to="/" />
+    } else {
+        return (
+            <>
+                <Container>
+                    <label htmlFor="btn-upload">
+                        <input
+                            id="btn-upload"
+                            name="btn-upload"
+                            style={{ display: 'none' }}
+                            type="file"
+                            onChange={selectFile}
+                            multiple
+                        />
+                        <Button
+                            variant="primary"
+                            as= "span"
+                            className= "mb-3"
+                        >
+                            Choose up to 3 images:
+                </Button>
+                    </label>
+                    {imageFiles[0] ? <p>{imageFiles[0].name} <br/></p> : <p>Select a File</p>}
+                    {imageFiles[1] ? <p>{imageFiles[1].name} <br/></p> : <p>Select a File</p>}
+                    {imageFiles[2] ? <p>{imageFiles[2].name}</p> : <p>Select a File</p>}
+    
+                </Container>
+    
+                <Button
+                    variant="primary"
+                    disabled={!imageFiles}
+                    onClick={upload}
+                    className= "mb-3"
+                >
+                    Upload
+                </Button>
+    
+                <ProgressBar variant="primary" now={uploadProgress} className= "mb-3"/>
+    
+                { error ? <div> Whoops! </div> : null}
+            </>
+        );
+    }
 
-            </Container>
-
-            <Button
-                variant="primary"
-                disabled={!imageFiles}
-                onClick={upload}
-                className= "mb-3"
-            >
-                Upload
-            </Button>
-
-            <ProgressBar variant="primary" now={uploadProgress} className= "mb-3"/>
-
-            { error ? <div> Whoops! </div> : null}
-        </>
-    );
 }
