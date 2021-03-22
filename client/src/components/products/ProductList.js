7c46ab4f969d8d573709ab2c2f2f043f7f97cf66
Dylan Eastridge <theillusionofthegift@gmail.com>
2021-03-21 23:25:56 -0700

    fixed merge conflicts

--- client/src/components/products/ProductList.js
+++ client/src/components/products/ProductList.js
@@ -6,16 +6,6 @@ import Product from "./Product";
 export default function ProductList(props) {
     const products = props.products
 
-    return (
-        <Container>
-            <Col style={{margin: "100px"}}>
-                <Row xs={1} md={4} xl={12} className="text-center">
-                    {products.map((product) => (<Product  key={product._id} products={product} />))}
-                </Row>
-            </Col>
-        </Container>
-    );
-
     if (props.seller) {
         return (
             <Container>
