ddf8fde41c40a1f173d4dc73f0d0e5d70e85c67d
Dylan <theillusionofthegift@gmail.com>
2021-03-23 20:54:45 -0700

    fix

--- service/index.js
+++ service/index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
-require('dotenv').config({ path: './.env' });
 
 const morgan = require('morgan');
 const cors = require('cors');
